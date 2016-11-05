8b6be5fb34cf6e59b6e9755a9cf57c1c8961eb1b
root <root@LAPTOP-8OLO069G.dhcp.wustl.edu>
2016-11-05 00:11:38 -0500

    added ip_location.py

--- sixteen/activity_graph/ip_location.py
+++ sixteen/activity_graph/ip_location.py
@@ -1,5 +1,15 @@
+from django.contrib.gis.GeoIP2 import GeoIP2
 def ip_location (ip_address):
     # Return a list:
     # l[0] = latitude, l[1] = longitude
+    g = GeoIP2()
+	ip = request.META.get('REMOTE_ADDR', None)
+	if ip:
+    	#city = g.city(ip)['city']
+    	lat_long = g.coords(ip)
+	else:
+    	#city = 'St. Louis' # default city
+    	lat_long = (38.6270, 90.1994)
+    return lat_long
 
     
