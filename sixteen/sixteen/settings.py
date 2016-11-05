2fa16fe9f5e967074cf04c1a204ee40552303db8
Jared Fernandez <fern.jared@gmail.com>
2016-11-05 10:14:51 -0500

    Djanko Part 2

--- sixteen/sixteen/settings.py
+++ sixteen/sixteen/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'activity_graph.apps.ActivityGraphConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'activity_graph.apps.ActivityGraphConfig'
+
 
 ]
 
