700db7a37b587359130d766fbffd18b8eaef0e7b
Jared Fernandez <fern.jared@gmail.com>
2016-11-05 02:37:30 -0500

    Distance Calc

--- sixteen/activity_graph/gis.py
+++ sixteen/activity_graph/gis.py
@@ -1,4 +1,6 @@
 from django.contrib.gis.GeoIP2 import GeoIP2
+from geopy.distance import great_circle
+
 def ip_location ():
 	#given an IP address, returns latitude and longitude of that IP address
     # Return a tuple:
@@ -13,4 +15,6 @@ def ip_location ():
     	lat_long = (38.6270, 90.1994)
     return lat_long
 
-    
+def distanceCalc(lat_long1, business1):
+    lat_long2   = (business1.latitude, business1.longitude)
+    return (great_circle(lat_long1,lat_long2).miles)
