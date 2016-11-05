a3a82019698d2113199690a150c0c309c8d3f9ee
Jared Fernandez <fern.jared@gmail.com>
2016-11-04 23:50:51 -0500

    Functions added

---
+++ sixteen/activity_graph/dank_algo.py
@@ -0,0 +1,3 @@
+def dankAlgo (user1, busines1):
+    # Calculate the rating for business
+    
---
+++ sixteen/activity_graph/ip_location.py
@@ -0,0 +1,5 @@
+def ip_location (ip_address):
+    # Return a list:
+    # l[0] = latitude, l[1] = longitude
+
+    
