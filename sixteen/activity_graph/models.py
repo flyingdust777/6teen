eae9c4082b69b33257e45fa7322d9726e5d7d59d
Jared Fernandez <fern.jared@gmail.com>
2016-11-05 00:16:10 -0500

    Djanko :'(

--- sixteen/activity_graph/models.py
+++ sixteen/activity_graph/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from django.contrib.auth.models. import User
 
-class affinity (models.Model):
-    user        = models.OneToOneField(User, on_delete = models.CASCADE)
-
+class ratings (models.Model):
     # Location
     latitude    = models.FloatField()
     longitude   = models.FloatField()
@@ -12,7 +10,7 @@ class affinity (models.Model):
     cost        = models.FloatField()
 
     # Sports
-    aquatic     = models.FloatField()
+    aqua        = models.FloatField()
     novel       = models.FloatField()
     leisure     = models.FloatField()
     coolShit    = models.FloatField()
@@ -22,6 +20,12 @@ class affinity (models.Model):
     mount       = models.FloatField()
     water       = models.FloatField()
 
+    class Meta:
+        abstract = True
+
+class extendedUser(models.Model):
+    user        = models.OneToOneField(User, on_delete = models.CASCADE)
+
 class business (models.Model):
-    cost        = models.FloatField()
-    
+    yelp_rate   = models.FloatField()
+    address     = models.CharField()
--- sixteen/sixteen/settings.py
+++ sixteen/sixteen/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'activity_graph.apps.ActivityGraphConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
