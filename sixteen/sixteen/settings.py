3e1cc6bb9a0217471d4a4bf10c1f52241fcdae8d
Jared Fernandez <fern.jared@gmail.com>
2016-11-05 02:19:35 -0500

    FUCK YEAH DJANKO

--- sixteen/sixteen/settings.py
+++ sixteen/sixteen/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-
+    'activity_graph.apps.ActivityGraphConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
