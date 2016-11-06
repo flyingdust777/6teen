94547e29adfc96ae3e38d68476b8e94b1fe21c84
Jared Fernandez <fern.jared@gmail.com>
2016-11-06 01:27:55 -0500

    MOLES

--- sixteen/activity_graph/urls.py
+++ sixteen/activity_graph/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     url (r'^$', views.index, name = 'index'),               # Home
     url (r'^account/$', views.account, name = 'account'),   # Login
     url (r'^feed/$', views.feed, name = 'feed'),            # Feed
-    url (r'^(?P<business>[name]+)/$', views.business, name = 'business') # Detailed Info
 ]
